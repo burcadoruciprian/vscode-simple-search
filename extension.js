533271961f83bae551d6b4bd3d9dd559f20557b9
Ciprian Burca <ciprian.burca@outlook.com>
2017-04-27 12:16:50 +0300

    Fixed algorithm for computing the search query based on selection.

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
 // Constant strings 
 const CFG_SECTION = "simpleSearch";
 const CFG_QUERY = "QueryTemplate";
-const CMD_ID = "extension.simpleSearch";   
+const CMD_ID = "extension.simpleSearch";
 
 var vscode = require('vscode');
 
@@ -20,20 +20,33 @@ exports.deactivate = deactivate;
 
 // Launches the search URL in default browser
 function WebSearch() {
-  var query = GetSearchQuery();
-  if (!query)
+  var selectedText = GetSelectedText();
+  if (!selectedText)
     return;
+
+  var uriText = encodeURI(selectedText);
+
+  var simpleSearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
+  const queryTemplate = simpleSearchCfg.get(CFG_QUERY);
+  var query = queryTemplate.replace("%SELECTION%", uriText);
+
   vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(query));
 }
 
 // Creates the search URL based on the selection and the query template.
-function GetSearchQuery() {
-  var selectedText = vscode.window.activeTextEditor.document.getText();
-  if (!selectedText)
+function GetSelectedText() {
+  const documentText = vscode.window.activeTextEditor.document.getText();
+  if (!documentText)
     return '';
-  
-  var simpleSearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
-  const queryTemplate = simpleSearchCfg.get(CFG_QUERY);
-  var query = queryTemplate.replace("%SELECTION%", selectedText);
-  return query;
+
+  var activeSelection = vscode.window.activeTextEditor.selection;
+  if (activeSelection.isEmpty)
+    return '';
+
+  const selStartOffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.start);
+  const selEndOffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.end);
+
+  var selectedText = documentText.slice(selStartOffset, selEndOffset).trim();
+  selectedText = selectedText.replace(/\s\s+/g, ' ');
+  return selectedText;
 }
